fad9b3da4916b9b79c9c6500f16d539490154bab
Manuel <x.fuentealba.ortiz@gmail.com>
2024-01-08 12:19:41 -0300

    gifs-app-v1

--- src/app/gifs/pages/search-box/search-box.component.ts
+++ src/app/gifs/pages/search-box/search-box.component.ts
@@ -1,4 +1,5 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
+import { GifsService } from '../../services/gifs.service';
 
 @Component({
   selector: 'gifs-search-box',
@@ -16,13 +17,15 @@ export class SearchBoxComponent {
 @ViewChild('txtTagInput')
 public tagInput!: ElementRef<HTMLInputElement>
 
-  constructor(){}
+  constructor(private GifsService:GifsService){}
 
 
   searchTag(){
 
     const newTag = this.tagInput.nativeElement.value
-    console.log(newTag);
+    this.GifsService.searchTag(newTag);
+    this.tagInput.nativeElement.value=''
+
 
   }
 
---
+++ src/app/gifs/services/gifs.service.ts
@@ -0,0 +1,52 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GifsService {
+
+  private _tagsHistory: string[] = [];
+  private apiKey: string ='NfHzlrD2N0Ip682R4zG9EJfris69Mqh2';
+  private serviceUrl:string = 'https://api.giphy.com/v1/gifs'
+
+
+  constructor( private http:HttpClient) { }
+
+  get tagsHistory() {
+    return [...this._tagsHistory];
+  }
+
+  private organizeHistory(tag: string) {
+    tag = tag.toLowerCase();
+
+    if (this._tagsHistory.includes(tag)) {
+      this._tagsHistory = this._tagsHistory.filter( (oldTag) => oldTag!==tag)
+
+    }
+    this._tagsHistory.unshift(tag)
+    this._tagsHistory = this.tagsHistory.splice(0,10)
+  }
+
+  searchTag(tag: string):void {
+    if (tag.length === 0) return;
+    this.organizeHistory(tag)
+
+    const params = new HttpParams()
+    .set('api_key', this.apiKey)
+    .set('limit', '10')
+    .set('q', tag)
+
+    this.http.get(`${this.serviceUrl}/search?`,{params}).subscribe(resp =>{
+      console.log(resp);
+
+    })
+
+
+
+
+  }
+
+
+}
