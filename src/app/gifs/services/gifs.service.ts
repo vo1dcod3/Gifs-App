986c4fa3c65994441193c5c56369a17f8c3e8408
Manuel <x.fuentealba.ortiz@gmail.com>
2024-01-08 14:04:59 -0300

    gifs-app-v1

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -30,6 +30,13 @@ export class GifsService {
     }
     this._tagsHistory.unshift(tag)
     this._tagsHistory = this.tagsHistory.splice(0,10)
+    this.saveLocalStorage();
+  }
+
+  private saveLocalStorage():void{
+
+    localStorage.setItem('history',JSON.stringify (this._tagsHistory));
+
   }
 
   searchTag(tag: string):void {
@@ -46,7 +53,7 @@ export class GifsService {
       console.log(resp);
 
       this.gifsList = resp.data ;
-    })
+    });
 
 
   }
