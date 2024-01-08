6615c594598b5ad7eedadf33b73ed9abfb85a013
Manuel <x.fuentealba.ortiz@gmail.com>
2024-01-08 11:02:44 -0300

    gifs-app-v1

---
+++ src/app/gifs/pages/search-box/search-box.component.ts
@@ -0,0 +1,30 @@
+import { Component, ElementRef, ViewChild } from '@angular/core';
+
+@Component({
+  selector: 'gifs-search-box',
+  template: `
+  <h5>Buscar:</h5>
+  <input type="text"
+  class="form-control"
+  placeholder="Buscar"
+  (keyup.enter)="searchTag()"
+  #txtTagInput
+  >`
+
+})
+export class SearchBoxComponent {
+@ViewChild('txtTagInput')
+public tagInput!: ElementRef<HTMLInputElement>
+
+  constructor(){}
+
+
+  searchTag(){
+
+    const newTag = this.tagInput.nativeElement.value
+    console.log(newTag);
+
+  }
+
+
+}
